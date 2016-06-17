27be9cd383a08d6e9b8c9c945484a994b811803b
Daniel Micay <danielmicay@gmail.com>
2016-06-17 07:13:49 -0400

    set deny_new_usb feature to dynamic by default
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -375,6 +375,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_SYSTEM_PROPERTIES += ro.secure=1
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
   ADDITIONAL_SYSTEM_PROPERTIES += ro.control_privapp_permissions=enforce
+  ADDITIONAL_SYSTEM_PROPERTIES += persist.security.deny_new_usb=dynamic
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1
