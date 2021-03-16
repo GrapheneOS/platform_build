48ca6d4fae75440758614b97518629638330c4aa
Daniel Micay <danielmicay@gmail.com>
2021-03-16 02:05:40 -0400

    ignore tethering provisioning requirement

--- core/main.mk
+++ core/main.mk
@@ -267,6 +267,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_DEFAULT_PROPERTIES += security.perf_harden=1
   ADDITIONAL_DEFAULT_PROPERTIES += ro.control_privapp_permissions=enforce
   ADDITIONAL_DEFAULT_PROPERTIES += persist.security.deny_new_usb=dynamic
+  ADDITIONAL_DEFAULT_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
