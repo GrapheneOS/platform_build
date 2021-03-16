27e405c0e796f6ac65c5013aedda4dc513ecae07
Daniel Micay <danielmicay@gmail.com>
2021-03-16 02:05:40 -0400

    ignore tethering provisioning requirement
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- core/main.mk
+++ core/main.mk
@@ -376,6 +376,7 @@ ifneq (,$(user_variant))
   ADDITIONAL_SYSTEM_PROPERTIES += security.perf_harden=1
   ADDITIONAL_SYSTEM_PROPERTIES += ro.control_privapp_permissions=enforce
   ADDITIONAL_SYSTEM_PROPERTIES += persist.security.deny_new_usb=dynamic
+  ADDITIONAL_SYSTEM_PROPERTIES += net.tethering.noprovisioning=true
 
   ifeq ($(user_variant),user)
     ADDITIONAL_SYSTEM_PROPERTIES += ro.adb.secure=1
