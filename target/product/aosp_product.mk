405202d0bc5531e857ab270bf10257ae2eea7da6
flawedworld <flawedworld@flawed.world>
2021-09-29 04:29:05 +0100

    Only copy sample APNs if a device does not supply any.
    Controlled by OEM_APNS := true
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -35,5 +35,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifneq ($(OEM_APNS),true)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif
