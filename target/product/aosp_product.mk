9bf009448c14926cf7cf11715c8231fe71985510
flawedworld <flawedworld@flawed.world>
2021-09-29 04:29:05 +0100

    omit AOSP sample APN database by default

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -32,8 +32,3 @@ PRODUCT_PACKAGES += \
     messaging \
     PhotoTable \
     preinstalled-packages-platform-aosp-product.xml \
-
-# Telephony:
-#   Provide a APN configuration to GSI product
-PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
