b67955cbcde579f8c6a9341556ffee08dc83f254
Daniel Micay <danielmicay@gmail.com>
2022-08-18 13:38:55 -0400

    add bluetooth/sdk_sandbox to standard key mapping

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -1137,6 +1137,8 @@ def BuildKeyMap(misc_info, key_mapping_options):
           devkeydir + "/shared":   d + "/shared",
           devkeydir + "/platform": d + "/platform",
           devkeydir + "/networkstack": d + "/networkstack",
+          devkeydir + "/bluetooth": d + "/bluetooth",
+          devkeydir + "/sdk_sandbox": d + "/sdk_sandbox",
       })
     else:
       OPTIONS.key_map[s] = d
