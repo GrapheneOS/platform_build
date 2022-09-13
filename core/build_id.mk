c2a6e5cbcf6f62de01682ec01524f498956d3ab8
Daniel Micay <danielmicay@gmail.com>
2022-09-13 00:17:20 -0400

    use device-specific build ids

--- core/build_id.mk
+++ core/build_id.mk
@@ -18,4 +18,10 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-BUILD_ID=TP1A.220905.004.A2
+ifneq (,$(filter raven oriole,$(TARGET_PRODUCT)))
+    BUILD_ID=TP1A.220905.004.A1
+else ifneq (,$(filter barbet redfin bramble sunfish coral flame,$(TARGET_PRODUCT)))
+    BUILD_ID=TP1A.220905.004
+else
+    BUILD_ID=TP1A.220905.004.A2
+endif
