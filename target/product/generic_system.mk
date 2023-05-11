501bac720358c5b0f8f9e91e041b68027f37894d
Daniel Micay <danielmicay@gmail.com>
2023-05-11 13:25:34 -0400

    Revert "only enable APEX on 6th/7th gen Pixel devices"
    This reverts commit 0ae730f8662bf7466e9f6f633f107bcad2179c72.

--- target/product/generic_system.mk
+++ target/product/generic_system.mk
@@ -21,10 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_default.mk)
 # Add adb keys to debuggable AOSP builds (if they exist)
 $(call inherit-product-if-exists, vendor/google/security/adb/vendor_key.mk)
 
-# Enable updating of APEXes on 6th and 7th generation Pixel devices only
-ifneq (,$(filter lynx cheetah panther oriole raven bluejay, $(TARGET_PRODUCT)))
-    $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-endif
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Shared java libs
 PRODUCT_PACKAGES += \
