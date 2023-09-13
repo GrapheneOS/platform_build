d6a137e85e7dd86bd1086313776a31e44e673bba
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-09-13 10:53:50 +0300

    merge script/envsetup.sh into envsetup.sh
    Android Studio for Platform hardcodes `source build/envsetup.sh`.
    
    For script/envsetup.sh history, see
    https://github.com/GrapheneOS/script/commits/2023090600/envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -15,6 +15,11 @@
 # gettop is duplicated here and in shell_utils.mk, because it's difficult
 # to find shell_utils.make without it for all the novel ways this file can be
 # sourced.  Other common functions should only be in one place or the other.
+
+umask 022
+# use shell built-in instead of the separate program which is missing by default on some distros
+alias which='command -v'
+
 function _gettop_once
 {
     local TOPFILE=build/make/core/envsetup.mk
@@ -2118,3 +2123,15 @@ set_global_paths
 source_vendorsetup
 addcompletions
 
+export LANG=C.UTF-8
+export _JAVA_OPTIONS=-XX:-UsePerfData
+export BUILD_DATETIME=$(cat out/build_date.txt 2>/dev/null || date -u +%s)
+echo "BUILD_DATETIME=$BUILD_DATETIME"
+export BUILD_NUMBER=$(cat out/soong/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y%m%d00)
+echo "BUILD_NUMBER=$BUILD_NUMBER"
+export DISPLAY_BUILD_NUMBER=true
+export BUILD_USERNAME=grapheneos
+export BUILD_HOSTNAME=grapheneos
+
+alias adevtool='vendor/adevtool/bin/run'
+alias adto='vendor/adevtool/bin/run'
