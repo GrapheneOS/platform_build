1d3b3be976540c8a4fcae116fb420671ac9c7f93
Daniel Micay <daniel.micay@grapheneos.org>
2023-10-31 21:07:35 -0400

    use sdk_phone_x86_64 as the default target

--- core/envsetup.mk
+++ core/envsetup.mk
@@ -83,7 +83,7 @@ CORRECT_BUILD_ENV_SEQUENCE_NUMBER := 13
 # ---------------------------------------------------------------
 # The product defaults to generic on hardware
 ifeq ($(TARGET_PRODUCT),)
-TARGET_PRODUCT := aosp_arm
+TARGET_PRODUCT := sdk_phone_x86_64
 endif
 
 
