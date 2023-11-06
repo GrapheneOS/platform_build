ffdd674c94281cc01d90dfb13f86e728733c4b44
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2023-11-06 16:40:17 +0000

    Take OUT_DIR environment variable into account instead of hardwiring to out dir

--- envsetup.sh
+++ envsetup.sh
@@ -2125,9 +2125,9 @@ addcompletions
 
 export LANG=C.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
-export BUILD_DATETIME=$(cat out/build_date.txt 2>/dev/null || date -u +%s)
+export BUILD_DATETIME=$(cat ${OUT_DIR:-out}/build_date.txt 2>/dev/null || date -u +%s)
 echo "BUILD_DATETIME=$BUILD_DATETIME"
-export BUILD_NUMBER=$(cat out/soong/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y%m%d00)
+export BUILD_NUMBER=$(cat ${OUT_DIR:-out}/soong/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y%m%d00)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 export BUILD_USERNAME=grapheneos
