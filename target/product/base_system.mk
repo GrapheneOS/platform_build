7a5215d75108851dd887cd5a4456176f3a98bebb
inthewaves <inthewaves@pm.me>
2026-05-08 09:29:30 -0700

    secure paste: install test helper in debug builds
    Install the SecurePasteTestSystemApp module through PRODUCT_PACKAGES_DEBUG so userdebug and eng
    images include the preinstalled system app needed by frameworks/base SecurePasteTests to verify the
    system app exemption.

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -616,6 +616,7 @@ PRODUCT_PACKAGES_DEBUG := \
     unwind_reg_info \
     unwind_symbols \
     HardeningTestApp \
+    SecurePasteTestSystemApp \
 
 # The set of packages whose code can be loaded by the system server.
 PRODUCT_SYSTEM_SERVER_APPS += \
